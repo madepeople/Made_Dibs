82303b0805596b7f0b462df7d47c7c561b11912d
Jonathan Selander <jonathan@mondaycreative.se>
2013-08-16 12:10:08 +0200

    coding style

--- src/code/Dibs/Block/Paymentwindow/Redirect.php
+++ src/code/Dibs/Block/Paymentwindow/Redirect.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Block that renders an auto-submitting DIBS form which redirects the
  * customer to the DIBS gateway where the purchase happens
@@ -7,36 +8,37 @@
  */
 class Made_Dibs_Block_Paymentwindow_Redirect extends Mage_Core_Block_Abstract
 {
+
     protected function _toHtml()
     {
         $dibsPaymentWindow = Mage::getModel('made_dibs/payment_paymentwindow');
 
         $form = new Varien_Data_Form();
         $form->setAction(Made_Dibs_Model_Payment_Paymentwindow::PAYMENTWINDOW_URL)
-            ->setId('made_dibs_paymentwindow')
-            ->setName('made_dibs_paymentwindow')
-            ->setMethod('POST')
-            ->setUseContainer(true);
+                ->setId('made_dibs_paymentwindow')
+                ->setName('made_dibs_paymentwindow')
+                ->setMethod('POST')
+                ->setUseContainer(true);
 
         foreach ($dibsPaymentWindow->getCheckoutFormFields()->toArray() as $field => $value) {
             $form->addField($field, 'hidden', array(
                 'name' => $field,
                 'value' => $value
-                    ));
+            ));
         }
 
         $idSuffix = Mage::helper('core')->uniqHash();
         $submitButton = new Varien_Data_Form_Element_Submit(array(
-            'value'    => $this->__('Click here if you are not redirected within 10 seconds...'),
+            'value' => $this->__('Click here if you are not redirected within 10 seconds...'),
         ));
         $id = "submit_to_dibs_button_{$idSuffix}";
         $submitButton->setId($id);
         $form->addElement($submitButton);
         $html = '<html><body>';
-        $html.= $this->__('You will be redirected to the DIBS website in a few seconds.');
-        $html.= $form->toHtml();
-        $html.= '<script type="text/javascript">document.getElementById("made_dibs_paymentwindow").submit();</script>';
-        $html.= '</body></html>';
+        $html .= $this->__('You will be redirected to the DIBS website in a few seconds.');
+        $html .= $form->toHtml();
+        $html .= '<script type="text/javascript">document.getElementById("made_dibs_paymentwindow").submit();</script>';
+        $html .= '</body></html>';
 
         return $html;
     }
